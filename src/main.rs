69caee23a7408207f57157260918301b7072e408
Glub-Star <mrglub7809@gmail.com>
2025-08-08 15:28:19 +0100

    Added Code and stared making the game

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
 use bevy::prelude::*;
 use rand::Rng;
+use bevy::prelude::Interaction;
 
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .insert_resource(Config {
+            difficulty: 0,
+            difficulty_multiplier: 1.0,
+            speed_modifier: 1.0,
+            cheats_enabled: false,
+        })
+        .insert_resource(Player {
+            name: "Hero".to_string(),
+            health: 100,
+            max_health: 100,
+            damage: 10,
+            potion_size: 25,
+            coins: 0,
+            score: 0,
+        })
+        .add_state::<GameState>()
+        .add_system_set(SystemSet::on_update(GameState::MainMenu).with_system(menu_button_system))
+        .add_system_set(SystemSet::on_enter(GameState::Exploring).with_system(exploring))
+        .add_system_set(SystemSet::on_update(GameState::Exploring).with_system(display_stats))
         .run();
 }
 
@@ -50,7 +70,8 @@ fn setup_main_menu(mut commands: Commands, assets: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
     commands.spawn(NodeBundle {
         style: Style {
-            size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+            width: Val::Px(200.0),
+            eight: Val::Px(50.0),
             flex_direction: FlexDirection::Column,
             align_items: AlignItems::Center,
             justify_content: JustifyContent::Center,
@@ -63,20 +84,21 @@ fn setup_main_menu(mut commands: Commands, assets: Res<AssetServer>) {
         spawn_menu_button(parent, &assets, "Start Game");
         spawn_menu_button(parent, &assets, "Change Difficulty");
         spawn_menu_button(parent, &assets, "Exit Game");
-    })
+    });
 }
 
 fn spawn_menu_button(parent: &mut ChildBuilder, assets: &Res<AssetServer>, label: &str) {
     parent
         .spawn(ButtonBundle {
             style: Style {
-                size: Size::new(Val::Px(200.0), Val::Px(50.0)),
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
                 margin: UiRect::all(Val::Px(5.0)),
                 justify_content: JustifyContent::Center,
                 align_items: AlignItems::Center,
                 ..default()
             },
-            background_color: Color::DARK_GRAY.into(),
+            background_color: bevy::prelude::BackgroundColor(Color::rgb(0.25, 0.25, 0.25)),
             ..default()
         })
         .with_children(|parent| {
@@ -98,7 +120,7 @@ fn menu_button_system(
     mut next_state: ResMut<NextState<GameState>>,
 ) {
     for (interaction, children) in &mut interaction_query{
-        f *interaction == Interaction::Clicked {
+        if *interaction == Interaction::Pressed {
             let label = &text_query.get_mut(children[0]).unwrap().sections[0].value;
             match label.as_str() {
                 "Start Game" => next_state.set(GameState::Exploring),
@@ -122,7 +144,7 @@ fn exploring(mut next_state: ResMut<NextState<GameState>>) {
     }
 }
 
-fn dispplay_stats(player: Res<Player>, mut query: Query<&mut Text>) {
+fn display_stats(player: Res<Player>, mut query: Query<&mut Text>) {
     for mut text in &mut query{
         text.sections[0].value = format!(
             "Name: {}\nHealth: {}/{}\nDamage: {}\nCoins: {}\nScore: {}",
